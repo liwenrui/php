8b74421775b420a279405fc0abaf80f710ef0d4a
李文瑞 <liwenrui@liwenrui.com>
2017-08-01 09:46:35 +0800

    加入array_map闭包use

---
+++ others/array_map.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * 欢迎交流php  array_map回调+闭包+use
+ * @author  李文瑞 <164798922@qq.com>
+ * @link    http://php.net/manual/zh/function.array-map.php
+ */
+$useA = 'A';
+$useB = 'B';
+$useC = 'C';
+$array = array(1,2,3,4,5,6);
+$func  = function($value) use ($useA,$useB,$useC){
+    $useC = 'D';
+    return $value . $useA . $useB;
+};
+
+$test = array_map($func,$array);
+
+/**
+ @debug 李文瑞
+ */
+echo "test:";
+echo "<pre>";
+    print_r($test);
+echo "</pre>";
+echo '<hr>';
+/**
+ @debug 李文瑞
+ */
+echo "useC:";
+echo "<pre>";
+    print_r($useC);
+echo "</pre>";
+echo '<hr>';
